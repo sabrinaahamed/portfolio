f23fa041db27e38241cf91f5d220dfbc238c2283
sabrinaahamed <sabrinaahamed.official@gmail.com>
2026-06-05 18:00:28 +0600

    feat: restructure portfolio layout with new components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import Nabvar from './components/layouts/Nabvar'
+import Hero from './components/sections/Hero'
+import About from './components/sections/About'
+import Contact from './components/sections/Contact'
+import Footer from './components/layouts/Footer'
+import Skills from './components/sections/Skills'
+import Projects from './components/sections/Projects'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline ">
-    Hello world!
-  </h1>
-    </div>
+    <>
+      <Nabvar />
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+  
+    </>
   )
 }
 
---
+++ src/components/ui/Container.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Container = ({ children }) => {
+  return (
+    <div className="max-w-6xl mx-auto px-6">
+        {children}
+
+    </div>
+  )
+}
+
+export default Container
