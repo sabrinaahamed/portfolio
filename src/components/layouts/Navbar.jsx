06a6b3ec8859bf9cc8d7541bc520483eabb351ff
sabrinaahamed <sabrinaahamed.official@gmail.com>
2026-06-06 21:23:29 +0600

    feat: enhance Navbar with icon integration and improved layout

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 
+import {
+  FiHome,
+  FiUser,
+  FiCode,
+  FiFolder,
+  FiMail,
+} from "react-icons/fi";
+
 const navLinks = [
-  "Home",
-  "About",
-  "Skills",
-  "Projects",
-  "Contact",
+  {
+    icon: <FiHome />,
+    href: "#home",
+    label: "Home",
+  },
+  {
+    icon: <FiUser />,
+    href: "#about",
+    label: "About",
+  },
+  {
+    icon: <FiCode />,
+    href: "#skills",
+    label: "Skills",
+  },
+  {
+    icon: <FiFolder />,
+    href: "#projects",
+    label: "Projects",
+  },
+  {
+    icon: <FiMail />,
+    href: "#contact",
+    label: "Contact",
+  },
 ];
 
 const Navbar = () => {
@@ -13,50 +41,79 @@ const Navbar = () => {
     <nav
       className="
         fixed
-        right-8
         top-1/2
         -translate-y-1/2
         z-50
         hidden
-        lg:flex
+        xl:block
       "
+      style={{
+        right: "max(20px, calc((100vw - 1280px) / 2 - 80px))",
+      }}
     >
       <div
         className="
-          flex
-          flex-col
-          gap-4
-          bg-white/70
-          backdrop-blur-xl
-          p-3
+          bg-white/80
+          backdrop-blur-2xl
           rounded-full
-          shadow-xl
+          p-3
           border
           border-white
+          shadow-[0_20px_60px_rgba(0,0,0,0.08)]
         "
       >
-        {navLinks.map((item) => (
-          <a
-            key={item}
-            href={`#${item.toLowerCase()}`}
-            className="
-              w-12
-              h-12
-              rounded-full
-              flex
-              items-center
-              justify-center
-              text-sm
-              font-medium
-              hover:bg-violet-600
-              hover:text-white
-              transition-all
-              duration-300
-            "
-          >
-            {item[0]}
-          </a>
-        ))}
+        <div className="flex flex-col gap-3">
+          {navLinks.map((link) => (
+            <a
+              key={link.label}
+              href={link.href}
+              title={link.label}
+              className="
+                group
+                relative
+                w-12
+                h-12
+                rounded-full
+                flex
+                items-center
+                justify-center
+                text-lg
+                text-slate-600
+                hover:bg-violet-600
+                hover:text-white
+                transition-all
+                duration-300
+              "
+            >
+              {link.icon}
+
+              {/* Tooltip */}
+
+              <span
+                className="
+                  absolute
+                  right-16
+                  px-3
+                  py-1.5
+                  rounded-lg
+                  bg-slate-900
+                  text-white
+                  text-sm
+                  whitespace-nowrap
+                  opacity-0
+                  translate-x-2
+                  group-hover:opacity-100
+                  group-hover:translate-x-0
+                  transition-all
+                  duration-300
+                  pointer-events-none
+                "
+              >
+                {link.label}
+              </span>
+            </a>
+          ))}
+        </div>
       </div>
     </nav>
   );
